4e16909c00eaccf4290b9bc63e1b54aeaaceae8b
Luís Ribeiro <contato.ribeiroluis@gmail.com>
2023-02-21 14:22:18 -0300

    Improve API and score calculation system

--- src/components/GameOver/index.jsx
+++ src/components/GameOver/index.jsx
@@ -1,22 +1,21 @@
 import { useContext } from "react"; 
 import { QuizContext } from "../../context/quiz";
-
 import "./styles.css"; 
 import Welldone from "../../img/welldone.svg"
 
 const GameOver = () => {
-
   const [quizState, dispatch] = useContext(QuizContext);
-
-  const point = quizState.score
+  const { score, questions } = quizState;
+  const totalQuestions = questions.length;
+  const percentage = ((score / totalQuestions) * 100).toFixed(2);
 
   return (
     <div id="gameover">
       <h2>Fim de Jogo!</h2>
-      <p>Pontuação: { (point * 100) / quizState.questions.length } </p>
-      <p>Você acertou {quizState.score} de {quizState.questions.length} perguntas.</p>
+      <p>Pontuação: {percentage}%</p>
+      <p>Você acertou {score} de {totalQuestions} perguntas.</p>
       <img src={Welldone} alt="Fim do Quiz" />
-      <button onClick={()=> dispatch({ type: "NEW_GAME" }) }>Reniciar</button>
+      <button onClick={()=> dispatch({ type: "NEW_GAME" }) }>Reiniciar</button>
     </div>
   )
 }
--- src/data/questions.js
+++ src/data/questions.js
@@ -1,12 +1,7 @@
-const data = [
-  {
-    question: "O que é Vanilla JavaScript?",
-    options: [
-      "JavaScript puro",
-      "Uma biblioteca JavaScript",
-      "Um framework JavaScript",
-      "Um compilador de JavaScript",
-    ],
+const data = [  
+  { 
+    question: "O que é Vanilla JavaScript?",    
+    options: ["JavaScript puro","Uma biblioteca JavaScript","Um framework JavaScript","Um compilador de JavaScript"],
     answer: "JavaScript puro",
   },
   {
@@ -29,6 +24,36 @@ const data = [
       "Qual destas propriedades da a quantidade de elementos de um array?",
     options: ["qty", "length", "items", "index"],
     answer: "length",
+  },
+  {
+    question: "O que é uma closure em JavaScript?",
+    options: [
+      "Uma função que retorna outra função",
+      "Uma variável definida fora de uma função",
+      "Um objeto que permite que você crie instâncias de classes",
+      "Uma estrutura de dados que armazena pares chave-valor",
+    ],
+    answer: "Uma função que retorna outra função",
+  },
+  {
+    question: "Qual o operador utilizado para fazer a comparação de igualdade em valor e tipo em JavaScript?",
+    options: ["===", "==", "=", "!=="],
+    answer: "===",
+  },
+  {
+    question: "Qual o método utilizado para transformar uma string em um array em JavaScript?",
+    options: ["split()", "join()", "slice()", "pop()"],
+    answer: "split()",
+  },
+  {
+    question: "Qual é a saída do código a seguir?\n\nconsole.log(1 + '2' + 2)",
+    options: ["'122'", "'32'", "'14'", "'NaN'"],
+    answer: "'122'",
+  },
+  {
+    question: "Qual o método utilizado para adicionar um elemento ao final de um array em JavaScript?",
+    options: ["push()", "pop()", "unshift()", "shift()"],
+    answer: "push()",
   },
 ];
 
