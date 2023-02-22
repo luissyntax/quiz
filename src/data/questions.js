cfa45804830b5b2eb2026743f222c69ad88c071f
Luís Ribeiro <contato.ribeiroluis@gmail.com>
2023-02-22 15:51:08 -0300

    Added new questions to the API

--- src/data/questions.js
+++ src/data/questions.js
@@ -54,7 +54,66 @@ const data = [
     question: "Qual o método utilizado para adicionar um elemento ao final de um array em JavaScript?",
     options: ["push()", "pop()", "unshift()", "shift()"],
     answer: "push()",
-  },
+  },{
+    question: "Qual é a diferença entre uma arrow function e uma função regular em JavaScript?",
+    options: [
+    "Arrow functions não podem ter parâmetros",
+    "Arrow functions não podem ser aninhadas",
+    "Arrow functions não possuem contexto de 'this'",
+    "Arrow functions não podem ser chamadas recursivamente",
+    ],
+    answer: "Arrow functions não possuem contexto de 'this'",
+    },
+    {
+    question: "O que é hoisting em JavaScript?",
+    options: [
+    "Um mecanismo que previne que variáveis sejam sobrescritas",
+    "Um mecanismo que evita a ocorrência de erros de sintaxe",
+    "Um mecanismo que permite o acesso a variáveis antes de sua declaração",
+    "Um mecanismo que garante que todas as variáveis sejam declaradas antes de serem usadas",
+    ],
+    answer: "Um mecanismo que permite o acesso a variáveis antes de sua declaração",
+    },
+    {
+    question: "Qual é a diferença entre let e var em JavaScript?",
+    options: [
+    "let é utilizado para declaração de variáveis globais, enquanto var é utilizado para variáveis locais",
+    "let é utilizado para variáveis que não podem ser reatribuídas, enquanto var pode ser reatribuída",
+    "let é uma palavra-chave mais antiga que var e não é mais utilizada em versões modernas de JavaScript",
+    "let é utilizada para declaração de variáveis locais, enquanto var é utilizada para variáveis globais",
+    ],
+    answer: "let é utilizado para variáveis que não podem ser reatribuídas, enquanto var pode ser reatribuída",
+    },
+    {
+    question: "O que é o operador ternário em JavaScript?",
+    options: [
+    "Um operador que realiza a multiplicação de dois valores",
+    "Um operador que compara dois valores e retorna um booleano",
+    "Um operador que permite atribuir um valor a uma variável com base em uma condição",
+    "Um operador que permite criar loops em JavaScript",
+    ],
+    answer: "Um operador que permite atribuir um valor a uma variável com base em uma condição",
+    },
+    {
+    question: "O que é o DOM em JavaScript?",
+    options: [
+    "Um conjunto de regras de sintaxe que definem como o JavaScript deve ser escrito",
+    "Um conjunto de bibliotecas que permitem a criação de animações em JavaScript",
+    "Uma interface de programação de aplicações que permite a integração de JavaScript com outras linguagens",
+    "A estrutura em árvore que representa os elementos HTML de uma página e que pode ser manipulada com JavaScript",
+    ],
+    answer: "A estrutura em árvore que representa os elementos HTML de uma página e que pode ser manipulada com JavaScript",
+    },
+    {
+    question: "O que é o evento 'click' em JavaScript?",
+    options: [
+    "Um evento que é disparado quando o usuário clica em um elemento da página",
+    "Um evento que é disparado quando o usuário carrega a página",
+    "Um evento que é disparado quando o usuário digita algo no teclado",
+    "Um evento que é disparado quando o usuário faz scroll na página",
+    ],
+    answer: "Um evento que é disparado quando o usuário clica em um elemento da página",
+    }
 ];
 
 export default data;
